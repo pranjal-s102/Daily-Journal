ff48a93d7c8d386713a5b907cbe30e0781792654
Pranjal <psingh5_be19@thapar.edu>
2022-07-16 21:53:22 +0530

    finished the blogsite with db

--- app.js
+++ app.js
@@ -1,26 +1,46 @@
 const express=require("express");
 const app=express();
 const _ =require("lodash");
+const ejs = require("ejs");
 const bodyParser=require("body-parser");
 const port=process.env.PORT||3000;
 const mongoose=require("mongoose");
-mongoose.connect("mongodb://localhost:27017/blogDB");
-const
+//const popup = require('popups');
+const alert = require('alert');
+const postSchema={
+    title:{
+      type:String,
+      unique:true,
+      dropDups:true
+    },
+    content:String
+};
+const Post = mongoose.model("Post",postSchema);
+
+
 app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs');
 app.use(express.static("public"));
-var posts=[];
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 app.use(express.static(__dirname + '\\public'));
+
+
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 
 
+
+
 app.get("/",function(req,res){
-  res.render("home",{bodyText:homeStartingContent,
-  posts:posts});
+  Post.find({},function(err,posts){
+    res.render("home",{bodyText:homeStartingContent,
+    posts:posts
+  });
 });
+});
+
 
 app.get("/about",function(req,res){
   res.render("about",{bodyText:aboutContent});
@@ -38,30 +58,38 @@ app.post("/compose",function(req,res)
 {
   let titleArea=req.body.composeTitle;
   let textArea=req.body.composeText;
-  var post={
-    title:titleArea,
-    text:textArea
-  };
-  posts.push(post);
-  res.redirect("/");
+  const post = new Post({
+    title:_.capitalize(titleArea),
+    content:textArea
+  });
+
+  post.save(function(err){
+    if(!err)
+    {
+      res.redirect("/");
+    }
+    else{
+      alert("Please enter a unique title");
+      console.log("Please enter a unique title");
+      res.redirect("/compose");
+    }
+  });
+
+  //posts.push(post);
 });
 
 
 app.get("/posts/:postName",function(req,res){
-  //console.log(req.params.postName);
   let y=_.lowerCase(req.params.postName);//takes care of the kababcase or lower upper case issues
-  for(let i=0;i<posts.length;i++)
-  {
-    if(_.lowerCase(posts[i].title)==y)
-    {
-      res.render("post",{title:posts[i].title, bodyText:posts[i].text});
-    }
-    else
-    {
-      res.redirect("/");
-    }
-  }
-})
+  const requestedPostId = req.params.postName;
+  Post.findOne({title:requestedPostId},function(err,post){
+    console.log("Found");
+    res.render("post",{
+      title:post.title,
+      bodyText: post.content
+    });
+  });
+});
 
 
 app.listen(port,function(){
